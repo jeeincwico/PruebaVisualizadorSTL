4b6ff79a65a1b079b2d7dfca65d77604c61eeb50
Carlos3ds <110524613+jeeincwico@users.noreply.github.com>
2024-01-23 20:16:07 -0600

    Add files via upload

---
+++ precache-manifest.b89d7c1b2c405cec18a3fb546fecd98d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f08a9969b7efda9450",
+    "url": "/static/css/main.2bdb4be9.chunk.css"
+  },
+  {
+    "revision": "72f08a9969b7efda9450",
+    "url": "/static/js/main.eb321f25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "842ebf2e891c96af6ce1",
+    "url": "/static/css/2.0325593e.chunk.css"
+  },
+  {
+    "revision": "842ebf2e891c96af6ce1",
+    "url": "/static/js/2.6d782e8c.chunk.js"
+  },
+  {
+    "revision": "79c88011d38e319063e9ebd1602f88f7",
+    "url": "/static/media/tecnolab.79c88011.jpg"
+  },
+  {
+    "revision": "f85a1bdeb4c88eb855fc844a04dcdca1",
+    "url": "/static/media/galeria1.f85a1bde.jpg"
+  },
+  {
+    "revision": "713bc0880494e539e01c0372cffcadfa",
+    "url": "/static/media/galeria2.713bc088.jpg"
+  },
+  {
+    "revision": "6c19a8f1b75be245a4ef18328e939f7a",
+    "url": "/static/media/galeria3.6c19a8f1.jpg"
+  },
+  {
+    "revision": "79c88011d38e319063e9ebd1602f88f7",
+    "url": "/static/media/galeria4.79c88011.jpg"
+  },
+  {
+    "revision": "a8b9577c47c3bc89cef743bb036b74fb",
+    "url": "/static/media/artrosis1.a8b9577c.png"
+  },
+  {
+    "revision": "412b3331c9ce8ee57ef7e37a6850b1c2",
+    "url": "/static/media/artrosis2.412b3331.png"
+  },
+  {
+    "revision": "a94c2dc003f476b6904ad646e712e31e",
+    "url": "/static/media/artrosis3.a94c2dc0.png"
+  },
+  {
+    "revision": "487b36e563b57d09a86419c7f560f621",
+    "url": "/static/media/artrosis4.487b36e5.png"
+  },
+  {
+    "revision": "6ebd9c3612d51b2ef5e825e26aed7ab6",
+    "url": "/static/media/artrosis5.6ebd9c36.png"
+  },
+  {
+    "revision": "e2c9ab4cc40f995fc31bb8ad4c194190",
+    "url": "/static/media/artrosis7.e2c9ab4c.png"
+  },
+  {
+    "revision": "f80c3184a3219ea4f435c01d0666aa83",
+    "url": "/static/media/artrosis8.f80c3184.png"
+  },
+  {
+    "revision": "7548be01804b63fe03ed725fc88e07fa",
+    "url": "/static/media/artrosis6.7548be01.png"
+  },
+  {
+    "revision": "12e461d8ceacf513a3422458bcc92872",
+    "url": "/static/media/columna_vertebral.12e461d8.png"
+  },
+  {
+    "revision": "cf3d0fa90592fd919253fa4841cd39df",
+    "url": "/static/media/columna_lumbar.cf3d0fa9.png"
+  },
+  {
+    "revision": "804cc79b75f81bc226dacf5f90ac0a17",
+    "url": "/static/media/vertebra_lumbar.804cc79b.png"
+  },
+  {
+    "revision": "5a084cd76e2c042aba22339b724fcd49",
+    "url": "/static/media/torax1.5a084cd7.jpg"
+  },
+  {
+    "revision": "1e472156407f0a4d273dc00c36fcafbf",
+    "url": "/static/media/material.1e472156.mtl"
+  },
+  {
+    "revision": "1036309360a6ebceb85fd7d172e884b2",
+    "url": "/static/media/pelvis.10363093.obj"
+  },
+  {
+    "revision": "802feb1cbc0e88851aff7c3dc8f6207c",
+    "url": "/static/media/protesis.802feb1c.obj"
+  },
+  {
+    "revision": "31de9a340b4eeae33f3c34290d2c6c9b",
+    "url": "/index.html"
+  }
+];
